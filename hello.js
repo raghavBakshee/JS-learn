efcfccb1d70103a4f8c228fecd530a2df91c59da
Raghav Bakshee <raghav_bakshee@yahoo.in>
2024-01-07 07:26:25 +0000

    js learn

--- hello.js
+++ hello.js
@@ -1,6 +1,15 @@
+"use strict"; // treat all JS code as newer version
 console.log("Hello World!")
-console.log("Hello World!")
+let accountPassword = "Hello"
+let notAssigned
+console.log(accountPassword, '123'+accountPassword);
+//console.table([accountPassword])
+console.log(typeof "Hello");
+console.log(typeof notAssigned);
 
+let userOne = { //Objects
+    email: "abc@gmail.com",
+    password: "Hello"
+}
 
-
-console.log("Hello World!")
+console.log(userOne);
