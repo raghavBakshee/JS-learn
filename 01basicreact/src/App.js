a5aa2c876064cd5df5280e20bfd0b0bfd60a5701
raghavBakshee <raghav_bakshee@yahoo.in>
2024-04-08 00:16:18 +0530

    setup basics react

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,11 @@
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1> Chai aur react | Raghav Bakshee</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,18 @@
+
+
+function App(){
+
+  return (
+    <>
+      <h1>Vite + React By Raghav Bakshee</h1>
+        <button>
+          count is 0
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR - Testing
+        </p>
+    </>
+  )
+}
+
+export default App
